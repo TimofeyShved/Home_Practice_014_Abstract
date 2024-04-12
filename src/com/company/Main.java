a21292e6da991efd3b5ba3ab19f64ecf485199c5
TimofeyShved <timofeya123@inbox.ru>
2024-04-12 20:39:28 +0600

    main code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,48 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Object[] objects = new Object[]{"zzz", 1, 34, null, 2.5, null, "4"};
+
+	    objects = Main.filterChain(objects);
+
+        for (Object o : objects){
+            System.out.println(o);
+        }
+    }
+
+    public static Object[] filterChain(Object[] old){
+        int offset = 0;
+        List list = new ArrayList();
+
+        for (Object o : old){
+            if (!new FilterNull().apply(o)){
+                list.add(o);
+            }
+        }
+
+        return list.toArray(new Object[list.size()]);
     }
 }
 
 
+// интерфейс Filter c методом apply(Object o), то есть мы проверяем что существует объект
+interface Filter {
+    boolean apply(Object o);
+}
 
+class FilterNull implements  Filter{
+
+    @Override
+    public boolean apply(Object o) {
+        if (o == null){
+            return true;
+        }
+        return false;
+    }
+}
